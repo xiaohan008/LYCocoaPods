808a70d9ddeeb084452de282117d9461f4652bcb
liuyang1 <liuyang1.uh@haier.com>
2021-11-08 18:38:00 +0800

    修改资源文件路径

---
+++ TestLibOne/HETestLib/Classes/LocalNotificationViewController.h
@@ -0,0 +1,16 @@
+//
+//  LocalNotificationViewController.h
+//  OCDemo
+//
+//  Created by user on 2021/11/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocalNotificationViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
